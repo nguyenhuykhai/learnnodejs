5da49a86d8fd2952ff0662dd1b65d6b062bf633d
Nguyen Huy Khai <nguyenhuykhaipch@gmail.com>
2023-08-03 19:38:24 +0700

    LESSON 8: ROUTING

---
+++ src/controller/homeController.js
@@ -0,0 +1,8 @@
+let getHomepage = (req,res) => {
+    //Viết Logic ở đây
+    return res.render('test/index.ejs')
+}
+
+module.exports = {
+    getHomepage
+}
--- src/server.js
+++ src/server.js
@@ -1,18 +1,16 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine.js';
 import {} from 'dotenv/config'
+import initWebRoute from './route/web.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+//SETUP VIEW ENGINE
 configViewEngine(app);
 
-app.get('/', (req, res) => {
-    res.render('test/index.ejs')
-})
-app.get('/about', (req, res) => {
-    res.send(`I'm Khải`)
-})
+//INIT WEB ROUTE
+initWebRoute(app);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
