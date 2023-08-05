4ade787246093660d044789fd4ff99c30b4f739d
Nguyen Huy Khai <nguyenhuykhaipch@gmail.com>
2023-08-05 16:39:05 +0700

    Lesson 15.2: Upload multiple file

--- src/route/web.js
+++ src/route/web.js
@@ -1,46 +1,77 @@
 import express from "express";
-import multer from 'multer';
-import path from 'path';
-import homeController from '../controller/homeController.js'
-var appRoot = require('app-root-path');
+import multer from "multer";
+import path from "path";
+import homeController from "../controller/homeController.js";
+var appRoot = require("app-root-path");
 let router = express.Router();
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, appRoot + "/src/public/image/");
-    },
+  destination: function (req, file, cb) {
+    cb(null, appRoot + "/src/public/image/");
+  },
 
-    // By default, multer removes file extensions so let's add them back
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
+  // By default, multer removes file extensions so let's add them back
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 
 const imageFilter = function (req, file, cb) {
-    // Accept images only
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-        req.fileValidationError = 'Only image files are allowed!';
-        return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true);
+  // Accept images only
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
 };
 
 let upload = multer({ storage: storage, fileFilter: imageFilter });
 
-const initWebRoute = (app) => {
-    router.get('/', homeController.getHomepage);
-    router.get('/detail/user/:id', homeController.getDetailPage);
-    router.post('/create-new-user', homeController.createNewUser);
-    router.post('/delete-user', homeController.deleteUser);
-    router.get('/edit-user/:id', homeController.getEditPage);
-    router.post('/update-user', homeController.postUpdateUser);
-    router.get('/upload', homeController.getUploadFilePage);
-    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
-    router.get('/about', (req, res) => {
-        res.send(`I'm Khải`)
-    })
+let upload1 = multer({ storage: storage, fileFilter: imageFilter }).array(
+  "multiple_images",
+  3
+);
 
-    return app.use('/', router)
-}
+const initWebRoute = (app) => {
+  router.get("/", homeController.getHomepage);
+  router.get("/detail/user/:id", homeController.getDetailPage);
+  router.post("/create-new-user", homeController.createNewUser);
+  router.post("/delete-user", homeController.deleteUser);
+  router.get("/edit-user/:id", homeController.getEditPage);
+  router.post("/update-user", homeController.postUpdateUser);
+  router.get("/upload", homeController.getUploadFilePage);
+  router.post(
+    "/upload-profile-pic",
+    upload.single("profile_pic"),
+    homeController.handleUploadFile
+  );
+  router.post(
+    "/upload-multiple-images",
+    (req, res, next) => {
+      upload1(req, res, (err) => {
+        if (
+          err instanceof multer.MulterError &&
+          err.code === "LIMIT_UNEXPECTED_FILE"
+        ) {
+          //handle multer file limit error
+          res.send("LIMIT_UNEXPECTED_FILE");
+        } else if (err) {
+          res.send(err);
+        } else {
+          next();
+        }
+      });
+    },
+    homeController.handleUploadMultipleFiles
+  );
+  router.get("/about", (req, res) => {
+    res.send(`I'm Khải`);
+  });
+
+  return app.use("/", router);
+};
 
 export default initWebRoute;
