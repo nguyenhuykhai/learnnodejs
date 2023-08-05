64bb8010bef02517cfcefda725b0ba4673c3bf45
Nguyen Huy Khai <nguyenhuykhaipch@gmail.com>
2023-08-05 17:15:54 +0700

    Lesson 16: Middleware

--- src/server.js
+++ src/server.js
@@ -5,10 +5,12 @@ import initWebRoute from './route/web.js';
 import initAPIRoute from './route/api.js'
 
 // import connection from './config/connectDB.js'
+var morgan = require('morgan')
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(morgan('combined'));
 app.use(express.urlencoded({extended: true }));
 app.use(express.json());
 
@@ -20,6 +22,11 @@ initWebRoute(app);
 
 //INIT API ROUTE
 initAPIRoute(app);
+
+//handle 404 not found
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
